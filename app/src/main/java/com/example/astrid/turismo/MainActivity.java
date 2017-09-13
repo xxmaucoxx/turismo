7a20ba5650035a8786325d0b6c031abd4ab10e52
btkira <btr_manuel2@hotmail.com>
2017-09-12 19:45:11 -0500

    MenuBotton
    Wiii

--- app/src/main/java/com/example/astrid/turismo/MainActivity.java
+++ app/src/main/java/com/example/astrid/turismo/MainActivity.java
@@ -3,7 +3,13 @@ package com.example.astrid.turismo;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -17,29 +23,58 @@ public class MainActivity extends AppCompatActivity {
     private TextView emailTextView;
     private TextView uidTextView;
 
+    private BottomNavigationView bottomNavigationView;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        nameTextView = (TextView) findViewById(R.id.nameTextView);
-        emailTextView = (TextView) findViewById(R.id.emailTextView);
-        uidTextView = (TextView) findViewById(R.id.uidTextView);
+        final Fragment perfilFragment = new SeccionPerfil();
+        final Fragment mapaFragment = new SeccionMapa();
+        final Fragment storeFragment = new SeccionStore();
+        final Fragment homeFragment = new SeccionInicio();
+
 
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
         if (user != null) {
-            String name = user.getDisplayName();
-            String email = user.getEmail();
-            Uri photoUrl = user.getPhotoUrl();
-            String uid = user.getUid();
 
-            nameTextView.setText(name);
-            emailTextView.setText(email);
-            uidTextView.setText(uid);
         } else {
             goLoginScreen();
         }
+
+        if (savedInstanceState == null) {
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.fragmentContainer, perfilFragment).commit();
+        }
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                FragmentManager fragmentManager = getSupportFragmentManager();
+
+                if (item.getItemId() == R.id.mapItem) {
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.fragmentContainer, mapaFragment).commit();
+                } else if (item.getItemId() == R.id.profileItem) {
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.fragmentContainer, perfilFragment).commit();
+                }else if (item.getItemId() == R.id.homeItem) {
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.fragmentContainer, homeFragment).commit();
+                }else if (item.getItemId() == R.id.storeItem) {
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.fragmentContainer, storeFragment).commit();
+                }
+                return true;
+            }
+        });
     }
 
     private void goLoginScreen() {
