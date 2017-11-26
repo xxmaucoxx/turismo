9c1c4f811aa854a28aa2573d717a7691ee0e25d7
btkira <btr_manuel2@hotmail.com>
2017-11-26 11:33:18 -0500

    cambios detalles
    ve

--- app/src/main/java/com/example/astrid/turismo/adaptadores/AdapterStore.java
+++ app/src/main/java/com/example/astrid/turismo/adaptadores/AdapterStore.java
@@ -2,10 +2,12 @@ package com.example.astrid.turismo.adaptadores;
 
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,9 +17,11 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.target.BitmapImageViewTarget;
 import com.example.astrid.turismo.R;
+import com.example.astrid.turismo.StorePage;
 import com.example.astrid.turismo.models.Point;
 import com.example.astrid.turismo.models.Post;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -36,10 +40,9 @@ public class AdapterStore extends RecyclerView.Adapter<AdapterStore.PointViewHol
     @Override
     public AdapterStore.PointViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_store,parent,false);
-        AdapterStore.PointViewHolder holder = new AdapterStore.PointViewHolder(v);
+        AdapterStore.PointViewHolder holder = new AdapterStore.PointViewHolder(v,this.context,points);
         return holder;
     }
-
     @Override
     public void onBindViewHolder(final PointViewHolder holder, int position) {
         Point point = points.get(position);
@@ -47,7 +50,6 @@ public class AdapterStore extends RecyclerView.Adapter<AdapterStore.PointViewHol
         holder.txt_card_category.setText(point.getCategoryStore());
         holder.txt_card_description.setText(point.getDescriptionStore());
         String url = point.getImgProfile();
-
         Glide.with(context)
                 .load(url)
                 .crossFade()
@@ -56,8 +58,6 @@ public class AdapterStore extends RecyclerView.Adapter<AdapterStore.PointViewHol
                 .into(holder.img_store);
 
     }
-
-
     @Override
     public int getItemCount() {
         return points.size();
@@ -68,19 +68,41 @@ public class AdapterStore extends RecyclerView.Adapter<AdapterStore.PointViewHol
         notifyDataSetChanged();
     }
 
-    public static class PointViewHolder extends RecyclerView.ViewHolder{
+    public static class PointViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        List<Point> points = new ArrayList();
+        Context ctx;
 
         ImageView img_store;
         TextView txt_card_name;
         TextView txt_card_category;
         TextView txt_card_description;
 
-        public PointViewHolder(View itemView) {
+        public PointViewHolder(View itemView , Context ctx, List<Point> points) {
             super(itemView);
+
+            this.points = points;
+            this.ctx = ctx;
+
             txt_card_name = (TextView) itemView.findViewById(R.id.txt_card_name);
             txt_card_category = (TextView) itemView.findViewById(R.id.txt_card_category);
             txt_card_description = (TextView) itemView.findViewById(R.id.txt_card_description);
             img_store = (ImageView) itemView.findViewById(R.id.img_store);
+
+            txt_card_name.setOnClickListener(this);
         }
+
+        @Override
+        public void onClick(View v) {
+            int position = getAdapterPosition();
+            Point point = this.points.get(position);
+            Log.i(TAG,"--------------------> El color es  :" + points.get(position).getColor());
+            Intent intent=new Intent(this.ctx, StorePage.class);
+            intent.putExtra("Tienda", points.get(position).getNameStore());
+            intent.putExtra("Color", points.get(position).getColor());
+            ctx.startActivity(intent);
+        }
+
+
     }
 }
